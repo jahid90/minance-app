6bf42f07b8512935beb714e0814d3ad81704cc17
Jahid <jahid90@users.noreply.github.com>
2021-02-06 15:28:37 +0530

    [client] add semantic-ui for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import { Container } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+    return <Container className="app">Hello World!</Container>;
 }
 
 export default App;
