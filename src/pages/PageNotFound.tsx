e3081b1e45bfe506ab22020187df42a41f00113b
Jahid <jahid90@users.noreply.github.com>
2021-02-13 17:38:37 +0530

    [client] style the not found page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './PageNotFound.css';
+
 function PageNotFound() {
     return (
-        <div>
+        <div className='not-found'>
             The requested page could not be found.
             <br />
             Go back to <Link to='/'>home</Link>.
