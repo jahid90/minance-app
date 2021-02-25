f548f4e1476d5db1e4682eeec6eba41c1d49186e
Jahid <jahid90@users.noreply.github.com>
2021-02-25 13:24:09 +0530

    [service] add quarterly interest payout instruction

--- src/main/java/io/jahiduls/minance/model/MaturityInstruction.java
+++ src/main/java/io/jahiduls/minance/model/MaturityInstruction.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 public enum MaturityInstruction {
 
     NONE("none"),
-    REINVEST_PRINCIPLE_AND_PAYOUT_INTEREST("reinvest-principle-and-payout-interest"),
     REINVEST_PRINCIPLE_AND_INTEREST("reinvest-principle-and-interest"),
-    PAYOUT_PRINCIPLE_AND_INTEREST("payout-principle-and-interest");
+    REINVEST_PRINCIPLE_AND_PAYOUT_INTEREST("reinvest-principle-and-payout-interest"),
+    PAYOUT_PRINCIPLE_AND_INTEREST("payout-principle-and-interest"),
+    PAYOUT_INTEREST_QUARTERLY("payout-interest-quarterly");
 
     @JsonValue @Getter
     private final String value;
